7d2e934ae2afe3c2f879b3e94e0dba2771684a92
Edgarvital <edgar.tecnico@hotmail.com>
2023-04-02 17:09:21 -0300

    Inicio da analise semantica

---
+++ analise_semantica.py
@@ -0,0 +1,70 @@
+import pandas as pd
+
+def analise(tabela_lexica, tabela_sintatica):
+    global tokens, lexemas, numLinhas, tokens_lines, lexemas_lines, tabela_simbolos
+    tokens = (tabela_lexica[tabela_lexica.columns[0:1:]]).values
+    lexemas = (tabela_lexica[tabela_lexica.columns[1:2:]]).values
+    numLinhas = (tabela_lexica[tabela_lexica.columns[2:3:]]).values
+
+    tokens_lines = create_line_tokens()
+    lexemas_lines = create_line_lexemas()
+    try:
+        loop_verificacao()
+    except Exception as e:
+        print_error('Erro de sintaxe')
+
+def loop_verificacao():
+    return 'peipei'
+
+def print_error(mensagem, linha=None):
+    if linha != None:
+        print("Erro de Sintaxe:", mensagem, 'Linha:', linha)
+    else:
+        print("Erro de Sintaxe:", mensagem)
+    exit()
+
+def create_line_lexemas():
+    lista_lexemas = {}
+    lexemas_line = []
+    linha_aux = [1]
+    last_position = -1
+    for index, linha in enumerate(numLinhas):
+        if (linha_aux == linha):
+            lexemas_line.append(lexemas[index, 0])
+        else:
+            lista_lexemas[linha_aux[0]] = lexemas_line
+            linha_aux = linha
+            lexemas_line = []
+            lexemas_line.append(lexemas[index, 0])
+
+    lista_lexemas[numLinhas[last_position, 0]] = [lexemas_line[last_position]]
+    return lista_lexemas
+
+def get_line_lexemas(linha):
+    try:
+        return lexemas_lines[linha].copy()
+    except Exception as e:
+        print('linha vazia ou não encontrada')
+
+def create_line_tokens():
+    lista_tokens = {}
+    tokens_line = []
+    linha_aux = [1]
+    last_position = -1
+    for index, linha in enumerate(numLinhas):
+        if (linha_aux == linha):
+            tokens_line.append(tokens[index, 0])
+        else:
+            lista_tokens[linha_aux[0]] = tokens_line
+            linha_aux = linha
+            tokens_line = []
+            tokens_line.append(tokens[index, 0])
+
+    lista_tokens[numLinhas[last_position, 0]] = [tokens_line[last_position]]
+    return lista_tokens
+
+def get_line_tokens(linha):
+    try:
+        return tokens_lines[linha].copy()
+    except Exception as e:
+        print('linha vazia ou não encontrada')
--- main.py
+++ main.py
@@ -1,13 +1,21 @@
 import analise_lexica
 import analise_sintatica
-
-#df.loc e df.iterrows funções importantes pra usar no dataframe
+import analise_semantica
 
 codigo = open('Codigo.txt', 'r')
 
-lexica = analise_lexica.analise(codigo)
-#print(lexica.to_string())
+tabela_lexica = analise_lexica.analise(codigo)
 
-sintatica = analise_sintatica.analise(lexica)
-print(sintatica)
+#print(tabela_lexica)
+
+
+tabela_sintatica = analise_sintatica.analise(tabela_lexica)
+
+print(tabela_sintatica)
+
+
+semantico = analise_semantica.analise(tabela_lexica, tabela_sintatica)
+
+#print(semantico)
+
 
